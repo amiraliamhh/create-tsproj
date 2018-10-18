8059721ee61f7c5e4f600771e7480d7fd917db7c
amirali <amirali@amirali-debian.amirali.com>
2018-10-18 23:26:02 +0330

    changed package version for publishing on npm

---
+++ helpers.d.ts
@@ -0,0 +1,6 @@
+export declare function createGitIgnore(folderName: string): Promise<any>;
+export declare function repoName(args: string[]): string | Error;
+export declare function setupBuildTasks(foldername: string): Promise<any>;
+export declare function getArg(argName: string): string | Error;
+export declare function hasArg(argName: string): boolean;
+export declare function addTslint(foldername: string): Promise<any>;
---
+++ msgs.d.ts
@@ -0,0 +1,2 @@
+/// <reference types="node" />
+export declare function loader(text: string): NodeJS.Timeout;
