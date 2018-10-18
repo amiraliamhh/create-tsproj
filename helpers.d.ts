6deb4cbf0a4840d891b50eff8d6c646f8611f20e
amirali <amirali@amirali-debian.amirali.com>
2018-10-18 23:22:30 +0330

    declaration files deleted

--- helpers.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function createGitIgnore(folderName: string): void;
-export declare function repoName(args: string[]): string | Error;
-export declare function setupBuildTasks(foldername: string): void;
-export declare function getArg(argName: string): string | Error;
-export declare function hasArg(argName: string): boolean;
-export declare function addTslint(foldername: string): Promise<any>;
